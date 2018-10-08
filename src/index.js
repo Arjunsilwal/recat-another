49cf4cb6e32609b5057c905b930f93f7058a3325
Arjun <asilwal222@gmail.com>
2018-10-08 09:48:02 +0545

    throttling change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, {Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
-
+import VideoList from './components/video_list';
+import VideoDetails from './components/video_detail';
 import SearchBar from './components/search_bar';
 
 
@@ -13,15 +15,33 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state= {videos:[]};
-    YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
-      this.setState({videos});
-    } );
+    this.state= {
+      videos:[],
+      selectedVideo: null
+    };
+this.videoSearch("Nepalisong");
   }
+
+videoSearch(term) {
+  YTSearch({key:API_KEY, term: term}, (videos) => {
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    });
+  } );
+}
+
   render(){
+
+const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return(
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetails video={this.state.selectedVideo} />
+        <VideoList
+        onVideoSelect ={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }
